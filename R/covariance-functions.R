889e1c1baef0e130f64ad2969992e9c21b55e56c
JimSkinner <jskinner.mail@gmail.com>
2017-04-25 11:29:23 +0100

    Added derivative of RQ covariance function and roxygen example usage (functioning as test code)

--- R/covariance-functions.R
+++ R/covariance-functions.R
@@ -29,13 +29,61 @@ cov.SE.d <- function(X, X2, beta, D=NA, ...) {
 #' @export
 #' @include util.R
 #' @import Matrix
+#' @import numDeriv
+#' @examples
+#' locs = matrix(c(0, 1.5), ncol=1)
+#' beta = c(2, 0.5, 1.5)
+#'
+#' # Numerical gradients
+#' grad.num  = numDeriv::grad(function(beta_) {
+#'   cov.RQ(locs, beta_)[1,2]
+#' }, beta)
+#'
+#' # Analytic gradients
+#' grads = cov.RQ.d(locs, beta)
+#' grad.real = vapply(1:3, function(i) grads[[i]][1,2], numeric(1))
+#'
+#' # Results are the same
+#' stopifnot(all.equal(grad.num, grad.real))
 cov.RQ <- function(X, beta, D=NA, ...) {
   # beta[1] log(sigma^2)
   # beta[2] log(lengthScale)
   # beta[3] log(alpha)
-  if (all(is.na(D))) { D = distanceMatrix(X, X2) }
-  D@x = exp(beta[1])*(1+(D@x^2)/(2*(exp(beta[2])^2)*exp(beta[3])))^(-exp(beta[3]))
+  stopifnot(length(beta)==3)
+  if (all(is.na(D))) { D = distanceMatrix(X) }
+  #D@x = exp(beta[1])*(1+(D@x^2)/(2*(exp(beta[2])^2)*exp(beta[3])))^(-exp(beta[3]))
+  D@x = exp(beta[1])*(1 + (D@x*D@x)*0.5*exp(-2*beta[2]-beta[3]))^(-exp(beta[3]))
   return(D)
+}
+
+#' Rational quadratic covariance function derivatives wrt hyperparameters
+#' @export
+#' @include util.R
+#' @import Matrix
+cov.RQ.d <- function(X, beta, D=NA, ...) {
+  stopifnot(length(beta)==3)
+  if (all(is.na(D))) { D = distanceMatrix(X) }
+
+  # Obtained using R 'grad' function
+  .expr1  <- exp(beta[1])
+  .expr3  <- D@x * D@x * 0.5
+  .expr7  <- exp(-2 * beta[2] - beta[3])
+  .expr8  <- .expr3 * .expr7
+  .expr9  <- 1 + .expr8
+  .expr10 <- exp(beta[3])
+  .expr11 <- -.expr10
+  .expr12 <- .expr9^.expr11
+  .expr13 <- .expr1 * .expr12
+  .expr15 <- .expr9^(.expr11 - 1)
+
+  dK1 <- D
+  dK2 <- D
+  dK3 <- D
+  dK1@x <- .expr13
+  dK2@x <- -(.expr1 * (.expr15 * (.expr11 * (.expr3 * (.expr7 * 2)))))
+  dK3@x <- -(.expr1 * (.expr12 * (log(.expr9) *
+           .expr10) + .expr15 * (.expr11 * .expr8)))
+  return(list(dK1, dK2, dK3))
 }
 
 cov.independent <- function(X, X2=NA, beta=c(), D=NA, max.dist=NA) {
