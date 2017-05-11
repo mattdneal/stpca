11561680a8097c0305e4eeea7729db0b1183306d
JimSkinner <jskinner.mail@gmail.com>
2017-05-11 22:01:18 +0100

    Function for synthesizing SPCA data

---
+++ R/synthesize-data.R
@@ -0,0 +1,38 @@
+#' Synthesize fake data from SPCA model
+#' @export
+#' @param n Number of samples to create
+#' @param k Latent dimensionality
+#' @param K prior covariance matrix
+#' @param noisesd standard deviation of noise added to data
+#' @import MASS
+synthesize_data <- function(n, k, K, noisesd=0) {
+  d   = ncol(K)
+  diag(K) = diag(K) + 1e-10
+  R_K = chol(K)
+  W   = t(R_K) %*% matrix(rnorm(d*k), nrow=d, ncol=k)
+  V   = mvrnorm(n=n, mu=rep(0, k), Sigma=diag(k))
+  X   = V %*% t(W) + matrix(rnorm(n*d, sd=noisesd), nrow=n, ncol=d)
+
+  return(list(X   = X,
+              W   = W,
+              V   = V))
+}
+
+#' Synthesize fake data from SPCA model
+#' @export
+#' @param n Number of samples to create
+#' @param k Latent dimensionality
+#' @param dim dimensions of grid to construct
+#' @param kern cvarinace function used to build the prior covariance matrix K
+#' @param noisesd standard deviation of noise added to data
+#' @import fields
+synthesize_data_kern <- function(n, k, dim, kern, noisesd=0) {
+  grid = make.surface.grid(lapply(dim, function(d_) seq(0, 1, length=d_)))
+  K    = kern(grid)
+  data.synth = synthesize_data(n, k, K, noisesd)
+  return(list(X    = data.synth$X,
+              W    = data.synth$W,
+              V    = data.synth$V,
+              grid = grid,
+              K    = K))
+}
