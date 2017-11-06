fd56c37969a305eddacaf0fbcc8684fa7caa2f7f
JimSkinner <j.r.skinner@warwick.ac.uk>
2017-11-06 18:03:33 +0000

    Test independent covar fun and check independant and SE can accept D

--- tests/testthat/testcovarFunctions.R
+++ tests/testthat/testcovarFunctions.R
@@ -1,5 +1,7 @@
 context("covariance functions")
 
+library(Matrix)
+
 locations = expand.grid(seq(-1, 1, length=10), seq(-1, 1, length=10))
 beta = log(c(1.5, 0.9))
 d = nrow(locations)
@@ -34,4 +36,52 @@ test_that("cov.SE.d matches numerical gradient", {
   }, x=beta)
   jac.analytic = vapply(dK, function(dKi) c(as.matrix(dKi)), numeric(d*d))
   expect_equal(jac.analytic, jac.num)
+})
+
+test_that("cov.SE can accept precomputed distance matrix", {
+  set.seed(1)
+  beta = rnorm(2)
+  K1 = cov.SE(locations, beta=beta)
+
+  D = distanceMatrix(locations) # Nonsparse: explicit zeroes
+  K2 = cov.SE(locations, beta=beta, D=D)
+
+  expect_equal(K1, K2)
+})
+
+test_that("cov.independant works as expected", {
+  set.seed(1)
+  beta = rnorm(1)
+  K = cov.independent(locations, beta=beta)
+  expect_is(K, "Matrix")
+  expect_that(all(is.finite(K)), is_true())
+  expect_equivalent(K, Diagonal(d, exp(beta)))
+})
+
+test_that("cov.independant.d matches numerical gradient", {
+  set.seed(1)
+  beta = rnorm(1)
+  dK = cov.independent.d(locations, beta=beta)
+
+  jac.num = c(jacobian(function(beta_) {
+    c(as.matrix(cov.independent(locations, beta=beta_)))
+  }, x=beta))
+  jac.analytic = c(as.matrix(dK))
+  expect_equivalent(jac.analytic, jac.num)
+})
+
+test_that("cov.independant can accept precomputed distance matrix", {
+  set.seed(1)
+  beta = rnorm(1)
+  K1 = cov.independent(locations, beta=beta)
+
+  D = distanceMatrix(locations) # Nonsparse: explicit zeroes
+  K2 = cov.independent(locations, beta=beta, D=D)
+
+  expect_equal(K1, K2)
+
+  D = distanceMatrix(locations, max.dist=0.5) # Sparse
+  K3 = cov.independent(locations, beta=beta, D=D)
+
+  expect_equal(K1, K3)
 })
