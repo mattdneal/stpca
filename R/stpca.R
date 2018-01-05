3ef6279e3624cfac7350b6bf7d3f1984e3c91949
JimSkinner <j.r.skinner@warwick.ac.uk>
2018-01-05 14:10:50 +0000

    set_beta and compute_gradient methods

--- R/stpca.R
+++ R/stpca.R
@@ -9,6 +9,7 @@ StpcaModel <- setRefClass("StpcaModel",
     d        = "integer",
     beta     = "numeric",
     K        = "Matrix",
+    KD       = "list",
     locs     = "matrix",
     covFn    = "function",
     covFnD   = "function",
@@ -18,11 +19,12 @@ StpcaModel <- setRefClass("StpcaModel",
     Vmean    = "matrix",
     Vvar     = "list",
     logEvidences  = "numeric",
-    logPosteriors = "numeric"
+    logPosteriors = "numeric",
+    LogEvidenceD  = "numeric"
   ),
   methods = list(
-    initialize = function(X=matrix(), k=1, beta0=numeric(0), locs=matrix(),
-                          covFn=NULL, covFnD=NULL, ...) {
+    initialize = function(X=matrix(nrow=0, ncol=0), k=1, beta0=numeric(0), locs=matrix(),
+                          covFn=function() NULL, covFnD=function() NULL, ...) {
       X      <<- X
       n      <<- nrow(X)
       k      <<- as.integer(k)
@@ -32,25 +34,23 @@ StpcaModel <- setRefClass("StpcaModel",
       covFn  <<- covFn
       covFnD <<- covFnD
 
-      ## Initialize theta (W, mu, sigSq) from PPCA
-      Xc <- scale(X, scale=FALSE) # Centered data: nxd
-      covar.svd <- svd(Xc/sqrt(n), nu=0, nv=k)
-      covar.eigval <- covar.svd$d^2
+      if (nrow(X)>0) {
+        ## Initialize theta (W, mu, sigSq) from PPCA
+        Xc <- scale(X, scale=FALSE) # Centered data: nxd
+        covar.svd <- svd(Xc/sqrt(n), nu=0, nv=k)
+        covar.eigval <- covar.svd$d^2
 
-      muHat    <<- attr(Xc, "scaled:center")
-      sigSqHat <<- sum(covar.eigval[-(1:k)])/(d-k)
-      WHat     <<- (covar.svd$v %*%
-                    diag(sqrt(covar.eigval[seq_len(k)] - sigSqHat)))
+        muHat    <<- attr(Xc, "scaled:center")
+        sigSqHat <<- sum(covar.eigval[-(1:k)])/(d-k)
+        WHat     <<- (covar.svd$v %*%
+                      diag(sqrt(covar.eigval[seq_len(k)] - sigSqHat)))
 
-      set_beta(beta0)
+        set_beta(beta0)
+      }
 
       callSuper(...)
     },
-    set_beta = function(beta, nIter=50) {
-      'Documentations for the method goes here'
-      beta <<- beta
-      K    <<- covFn(locs, beta=beta)
-
+    fit = function(nIter=50) {
       vals <- fit_stpca(X, WHat, muHat, sigSqHat, K, nIter)
 
       WHat          <<- vals$WHat
@@ -60,6 +60,18 @@ StpcaModel <- setRefClass("StpcaModel",
       logEvidences  <<- c(logEvidences,  vals$logEvidence)
       logPosteriors <<- c(logPosteriors, vals$logPosteriors)
 
+      return(.self)
+    },
+    set_beta = function(beta, nIter=50) {
+      'Documentations for the method goes here'
+      beta <<- beta
+      K    <<- covFn(locs, beta=beta)
+      fit()
+      return(.self)
+    },
+    compute_gradient = function() {
+      KD <<- covFnD(locs, beta=beta)
+      logEvidenceD <<- log_evidence_d(X, K, WHat, muHat, sigSqHat, beta, KD)
       return(.self)
     },
     tune_beta = function(nIter) {
