af650e3f70628680e8a8b02fa9cb0e9828f8e5f5
JimSkinner <j.r.skinner@warwick.ac.uk>
2018-01-05 14:09:43 +0000

    Docs

--- R/statistical-quantities.R
+++ R/statistical-quantities.R
@@ -25,6 +25,7 @@
 #' l1 = log_likelihood(X, W, mu, sigSq)
 #' l2 = log_likelihood(X, W%*%R, mu, sigSq)
 #' stopifnot(all.equal(l1, l2))
+#' @export
 log_likelihood <- function(X, W, mu, sigSq) {
   Xc = sweep(X, 2, mu)
   d = nrow(W)
@@ -41,6 +42,16 @@ log_likelihood <- function(X, W, mu, sigSq) {
   return(-0.5*(const + nLogDetC + trXCinvXt))
 }
 
+#' Compute a value proportional to the  expected complete log posterior:
+#'   E[ log p(\theta | X, V, \beta) | V ]
+#' \propto
+#'   E[ log p(X | V, \theta) | V ] + log p(\theta | \beta) + E[ log p(V) | V ]
+#'
+#' This is "propertional to" because we do not compute the normalising
+#' constant E[ log p(X, V) | V ]. This does not matter since this function is
+#' used to check that each EM M-step maximizes this quantity w.r.t \theta.
+#'
+#' @export
 complete_log_posterior <- function(X, V, Vvar, W, mu, sigSq, K) {
   require(mvtnorm)
   d = ncol(X)
