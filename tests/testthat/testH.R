dae7a6899ae35a176abdfb2b50b92c671f9a15c2
JimSkinner <j.r.skinner@warwick.ac.uk>
2018-01-09 09:50:36 +0000

    Test takes too long

--- tests/testthat/testH.R
+++ tests/testthat/testH.R
@@ -38,29 +38,3 @@ test_that("Building H triggers an informative error if K cannot be inverted", {
     compute_H(X, stpca$WHat, stpca$muHat, stpca$sigSqHat, K_)
   }, "Could not invert K")
 })
-
-test_that("diag of H matches numerical reconstruction on larget dataset", {
-  # This is also checking non-optimised params.
-  n     = 15
-  k     = 4
-  dim   = c(21, 21)
-  d     = prod(dim)
-  beta  = log(c(2, 0.2, 1e-5))
-  k_se  = Curry(cov.noisy.SE, beta=beta)
-  sigSq = 1.8
-
-  synth = synthesize_data_kern(n, k, dim, kern=k_se, noisesd=sqrt(sigSq))
-
-  Hw1diag.num = vapply(1:d, function(i) {
-    as.numeric(hessian(function(wi) {
-      W_ = synth$W
-      W_[i,1] = wi
-      -(log_likelihood(synth$X, W_, 0, sigSq) +
-        log_prior(synth$K, W_))
-    }, synth$W[i,1]))
-  }, numeric(1))
-
-  Hw1diag = diag(compute_H_W(synth$X, synth$W, 0, sigSq, synth$K)[[1]])
-
-  expect_equal(Hw1diag, Hw1diag.num)
-})
