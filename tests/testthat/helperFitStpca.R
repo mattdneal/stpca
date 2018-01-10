657da71fa0222e986fe88080da0e0aabd7fe85ae
JimSkinner <j.r.skinner@warwick.ac.uk>
2018-01-10 11:03:42 +0000

    Add tuned model for testing

--- tests/testthat/helperFitStpca.R
+++ tests/testthat/helperFitStpca.R
@@ -18,16 +18,16 @@ locs = dataset$grid
 X = as.matrix(dataset$X)
 
 beta0 = c(cov.SE.beta0(X, locs, k), log(1e-3))
-constr = list(
-  ineqA = rbind(diag(3), -diag(3)),
-  ineqB = c(-log(c(0.01, 0.01, 1e-5)),
-            log(c(100*crossprod(X[1,]),
-                             5, 10)))
-)
 
 stpca <- StpcaModel$new(X, k, beta0, locs, cov.noisy.SE, cov.noisy.SE.d, nIter=50)
 
 #beta0 = log(c(1, 0.6, 0.6, 1e-3))
 #stpca <- StpcaModel$new(X, k, beta0, locs, cov.noisy.MR, cov.noisy.MR.d, nIter=50)
 
-#stpca$tune_beta(constraints=constr)
+constr = list(
+  ineqA = rbind(diag(3), -diag(3)),
+  ineqB = c(-log(c(0.01, 0.01, 1e-5)),
+            log(c(100*crossprod(X[1,]),
+                             5, 10)))
+)
+stpcaTuned = stpca$copy()$tune_beta(constraints=constr)
