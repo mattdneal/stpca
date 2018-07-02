235cb2512eb2a6383ed9a8bed5de2c98617a749d
JimSkinner <j.r.skinner@warwick.ac.uk>
2018-07-02 21:47:04 +0100

    More unit tests for CV

--- tests/testthat/testCV.R
+++ tests/testthat/testCV.R
@@ -1,6 +1,19 @@
 context("Crossvalidation")
 
-expect_that("crossvalidating picks likely over unlikely beta", {
+expect_that("crossvalidating gives sensible output", {
   cvll <- mean(stpcaUp$crossvalidate()$ll)
-  expect_true(is.finite(cvll$ll))
+  expect_true(is.finite(cvll))
+})
+
+expect_that("crossvalidating picks most likely beta", {
+  stpcaCpy     <- stpcaUp$copy()$set_beta(beta+10)
+  cvllLikely   <- mean(stpcaUp$crossvalidate()$ll)
+  cvllUnlikely <- mean(stpcaCpy$crossvalidate()$ll)
+  expect_gt(cvllLikely, cvllUnlikely)
+})
+
+expect_that("crossvalidating does not change stpca object", {
+  stpcaCpy <- stpcaUp$copy()
+  stpcaCpy$crossvalidate(3, 3)
+  expect_equal(stpcaUp, stpcaCpy)
 })
